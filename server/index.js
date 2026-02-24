1daa0d4a5098001b6acadd222f7f9fac93d733ec
sakshimarkal <sakshimarkal66@gmail.com>
2026-02-24 10:33:35 +0530

    auth api added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose")
 const app = express()
 mongoose.connect(process.env.MONGO_URL)
 
+
+app.use("/api/auth", require("./routes/auth.routes.js"))
+
 app.use("/", (req, res) => {
     res.status(200).json({ message: `Task Manager Api Running in ${process.env.NODE_ENV} mode` })
 })
